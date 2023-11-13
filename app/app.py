a7c75ff3fc00d176c129a4dd1b676b82f3e78aa2
Kent Hua <pitchdarkice@gmail.com>
2023-11-13 18:58:10 +0000

    add chat history capability

--- app/app.py
+++ app/app.py
@@ -2,27 +2,50 @@ import requests
 import gradio as gr
 import os
 
-json_message = {
-  "model": os.environ["MODEL_ID"],
-  "messages": [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "What are the top 5 most popular programming languages? Please be brief."}
-  ],
-  "temperature": 0.7
-}
+model_id = os.environ["MODEL_ID"]
 
 def inference_interface(message, history, model_temperature):
-  json_message['messages'][1]["content"] = message
+
+  json_message = {
+    "model": model_id,
+    "messages": [],
+    "temperature": 0.7
+  }
+
+  print("* History: " + str(history))
+
+  system_message = {"role": "system", "content": "You are a helpful assistant."}
+  json_message["messages"].append(system_message)
   json_message['temperature'] = model_temperature
 
-  print("request" + str(json_message), flush=True)
+  if len(history) == 0:
+    # when there is no history
+    print("** Before adding messages: " + str(json_message['messages']))
+    new_user_message = {"role": "user", "content": message}
+    json_message['messages'].append(new_user_message)
+  else:
+    # we have history
+    print("** Before adding additional messages: " + str(json_message['messages']))
+    for item in history:
+      user_message = {"role": "user", "content": item[0]}
+      assistant_message = {"role": "assistant", "content": item[1]}
+      json_message["messages"].append(user_message)
+      json_message["messages"].append(assistant_message)
+    new_user_message = {"role": "user", "content": message}
+    json_message["messages"].append(new_user_message)
+      
+  print("*** Request" + str(json_message), flush=True)
   response = requests.post(os.environ["HOST"] + os.environ["CONTEXT_PATH"], json=json_message)
   json_data = response.json()
   output = json_data["choices"][0]["message"]["content"]
-  return output 
+  return output
 
 with gr.Blocks() as app:
+  html_text = "You are chatting with: " + model_id
+  gr.HTML(value=html_text)
+
   model_temperature = gr.Slider(minimum=0.1, maximum=1.0, value=0.7, label="Temperature", render=False)
+  
   gr.ChatInterface(
     inference_interface,
     additional_inputs=[
